05138ea699feed2018c1e27cab0ab06c36758e43
Ivanx95 <jaisen.95@gmail.com>
2019-09-10 15:42:34 -0500

    Update LabsComponent.js

--- js/components/LabsComponent.js
+++ js/components/LabsComponent.js
@@ -1,20 +1,20 @@
 
 
-import {prepareElement} from '../base/views/PrepareSwitch.js';
 
 
+import { render} from '../base/baseComponent.js';
 
-import {createView} from '../base/views/BaseViewPromise.js';
+
 
 
 export async function labs(fatherComponent){
   
-    prepareElement(fatherComponent);
+   
 
     var element  = await createView("js/components/LabsComponent.html");
 
     
-
+  render("js/components/LabsComponent.html", fatherComponent);
 
   
 }
