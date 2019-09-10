2ec4292955f79c4a382431bb31c498f61143a0c8
Ivanx95 <jaisen.95@gmail.com>
2019-09-10 15:42:49 -0500

    Update LabsComponent.js

--- js/components/LabsComponent.js
+++ js/components/LabsComponent.js
@@ -10,9 +10,6 @@ import { render} from '../base/baseComponent.js';
 export async function labs(fatherComponent){
   
    
-
-    var element  = await createView("js/components/LabsComponent.html");
-
     
   render("js/components/LabsComponent.html", fatherComponent);
 
