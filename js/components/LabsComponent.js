08f3858a3f6071deca9bd14b2c62980364c17586
Ivanx95 <jaisen.95@gmail.com>
2019-09-11 10:36:23 -0500

    Update LabsComponent.js

--- js/components/LabsComponent.js
+++ js/components/LabsComponent.js
@@ -16,7 +16,7 @@ export async function labs(fatherComponent){
 try {
    prepareElement(fatherComponent);
 
-  var element  = await createView("js/components/LabsComponent.html");
+   var element  = await createView("js/components/LabsComponent.html");
 
    fatherComponent.appendChild(element);
     
@@ -25,30 +25,29 @@ try {
     const col1 = await column();
     let mixFileInput = {".intro-content":fileInput};
     const card = new CardComp(col1,"","", "Image Search","",mixFileInput);
-   
+    const col2 = await column();
 
-   const col2 = await column();
-
-  const card2 = canvasCard();
-  col2.appendChild(card2);
+    const card2 = await canvasCard();
+    col2.appendChild(card2);
+  
     loadScript("https://docs.opencv.org/3.3.1/opencv.js", function(){
 
         var introColumns =element.querySelector(".intro-columns");
 
          introColumns.appendChild(col1);
          introColumns.appendChild(col2);
-        console.log('OpenCV fully loaded');
-        let openCvBanner =  fatherComponent.querySelector('#openCvBanner');
-        if(openCvBanner!=null){
-          openCvBanner.innerHTML="OpenCV fully loaded";
+         console.log('OpenCV fully loaded');
+         let openCvBanner =  fatherComponent.querySelector('#openCvBanner');
+         if(openCvBanner!=null){
+           openCvBanner.innerHTML="OpenCV fully loaded";
           
-          var imgElement = fatherComponent.querySelector(".is-4by3").childNodes[1];
+           var imgElement = fatherComponent.querySelector(".is-4by3").childNodes[1];
    
-        let inputFile =fileInput.querySelector('.file-input');
+           let inputFile =fileInput.querySelector('.file-input');
 
-        inputFile.addEventListener("change", (e) => {
-            imgElement.src = URL.createObjectURL(e.target.files[0]);
-        }, false);
+           inputFile.addEventListener("change", (e) => {
+              imgElement.src = URL.createObjectURL(e.target.files[0]);
+           }, false);
           
            let btnExecute = fileInput.querySelector('#btnAction');
            btnExecute.addEventListener("click", (e) =>{
