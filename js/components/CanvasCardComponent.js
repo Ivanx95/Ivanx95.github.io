84513ce2625acffe9c4af50e6bdb7a5f5cc00cd6
Ivanx95 <jaisen.95@gmail.com>
2019-09-11 10:41:45 -0500

    Update CanvasCardComponent.js

--- js/components/CanvasCardComponent.js
+++ js/components/CanvasCardComponent.js
@@ -3,12 +3,13 @@
 import {createView} from '../base/views/BaseViewPromise.js';
 
 
-export async function canvasCard( ){
+export async function canvasCard(title){
 
 
 
     let canvas = await  createView("js/components/CanvasCard.html");
-
+    let titleEl = canvas.querySelector(".intro-title");
+    titleEl.innerHTML=title;
 
     return canvas;
 
