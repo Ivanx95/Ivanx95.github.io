70e06bcbecfc6fd3581218eb994b891ea94a4ce6
Ivanx95 <jaisen.95@gmail.com>
2019-09-11 10:30:19 -0500

    Update CanvascardComponent.js

--- js/components/CanvascardComponent.js
+++ js/components/CanvascardComponent.js
@@ -1,1 +1,16 @@
 
+
+import {createView} from '../base/view/BaseViewPromise.js';
+
+
+
+export async function canvasCard( ){
+
+
+
+    let canvas = await  createView("js/base/views/Column.html");
+
+
+    return canvas;
+
+}
