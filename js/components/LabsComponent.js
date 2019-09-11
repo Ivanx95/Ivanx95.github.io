f744f273c97b5dccb5823be3a6ee1c35097a2cd3
Ivanx95 <jaisen.95@gmail.com>
2019-09-11 11:10:53 -0500

    Update LabsComponent.js

--- js/components/LabsComponent.js
+++ js/components/LabsComponent.js
@@ -46,7 +46,15 @@ try {
            let inputFile =fileInput.querySelector('.file-input');
 
            inputFile.addEventListener("change", (e) => {
-              imgElement.src = URL.createObjectURL(e.target.files[0]);
+            
+               var context = imgElement.getContext('2d');
+               var imageObj = new Image();
+
+               imageObj.onload = function() {
+                  context.drawImage(imageObj, 69, 50);
+               };
+                imageObj.src = URL.createObjectURL(e.target.files[0]);
+          
            }, false);
           
            let btnExecute = fileInput.querySelector('#btnAction');
