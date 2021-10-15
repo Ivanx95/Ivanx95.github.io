246ef3f26d2d2c3872e127edc821544285c64af8
Ivan <jaisen.95@gmail.com>
2021-10-15 13:53:12 -0500

    adjusted order of projects

--- js/components/ProjectsComponent.js
+++ js/components/ProjectsComponent.js
@@ -27,23 +27,25 @@ export async function projects(fatherComponent){
     
     element=element.querySelector(".intro-columns");
     
+    const col4 = await column();
     const col3 = await column();
     const col2 = await column();
     const col1 = await column();
 
 
-    
+    element.appendChild(col4);
     element.appendChild(col3);
     element.appendChild(col2);
     element.appendChild(col1);
+
    
     let cMixBtn = await buttonCMixUp();
 
-    card(col1,"springFx.png","springFx.png", "Spring javafx", "SimplePTV made with spring-boot-support and javafx 8");
     card(col1,"card_client.png","card_client.png", '<p>Vanilla js complete card pints paas <a href="http://mycard.host/">mycard </a></p>');
-    card(col2,"electron.png","electron.png", "SimplePTV Electron", "SimplePTV made with ElectronJS  Bulma CSS and Sequelize ");
+    card(col2,"springFx.png","springFx.png", "Spring javafx", "SimplePTV made with spring-boot-support and javafx 8");
+    card(col3,"electron.png","electron.png", "SimplePTV Electron", "SimplePTV made with ElectronJS  Bulma CSS and Sequelize ");
     let mixBtnNode = {".intro-content":cMixBtn};
-    card(col3,"java_c.png","java_c.png", "Java c++ legacy mix", "JVM controlled by a C program",mixBtnNode);     
+    card(col4,"java_c.png","java_c.png", "Java c++ legacy mix", "JVM controlled by a C program",mixBtnNode);     
 
     fatherComponent.appendChild(element);
         
